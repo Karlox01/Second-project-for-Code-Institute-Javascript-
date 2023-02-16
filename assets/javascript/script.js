e7576a40432b6b182706396894c16b3f86e93e3b
Karlox01 <orlandinikarlo@gmail.com>
2023-02-16 20:27:02 +0000

    implement start button and css reaction to javascript , before start no grid, Once start is clicked layered grid with questions added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,29 @@
+const startButton = document.getElementById('start');
+startButton.addEventListener('click', startGame)
+
+const nextButton = document.getElementById('next');
+let submitFlex = document.querySelector('.submit');
+const questionContainer = document.querySelector('.question-container');
+const container = document.querySelector('.container');
+
+
+
+
+
+
+
+
 function startGame() {
+    console.log('begin');
+    startButton.style.display ='none';
+    questionContainer.style.display ='revert';
+    container.style.display ='grid';
+    nextButton.style.display = 'initial';
+    submitFlex.style.justifyContent = 'flex-end';
+
+    
+    
+    
 
 }
 
