9ce098042cd25cb2773e5b9abdd95ee0f3eb9e57
Karlox01 <orlandinikarlo@gmail.com>
2023-02-19 20:17:46 +0000

    change to html side of things ,Add radio to the possible quiz options , Also changed javascript to simplify functions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,24 +1,71 @@
-const startButton = document.getElementById('start');
-startButton.addEventListener('click', startGame)
+let questions = [
+    {
+        "question": 'When did Napoleon Bonaparte become the Emperor of France.?',
+        "option1": '1705',
+        "option2": '1804',
+        "option3": '1920',
+        "option4": '1815',
+        "answer": "2"
+    },
+    {
+        "question": 'What year did Napoleon Bonaparte invade Russia?',
+        "option1": '1709',
+        "option2": '1812',
+        "option3": '1922',
+        "option4": '1821',
+        "answer": "2"
+    },
+    {
+        "question": 'Where was Napoleon exiled to, When he first abdicated the throne?',
+        "option1": 'Island of Elba',
+        "option2": 'Dutchy of Touissant',
+        "option3": 'Land of Rivia',
+        "option4": 'Saint Pierre',
+        "answer": "1"
+    },
+    {
+        "question": 'Who led the army that defeated Napoleon Bonaparte , At the battle of Waterloo?',
+        "option1": 'Arthur Wellesley , The duke of Wellington',
+        "option2": 'Geralt of Rivia',
+        "option3": 'Kaiser Henry the 3rd.',
+        "option4": 'Jean Pierre De La Rouge',
+        "answer": "1"
+    },
+    {
+        "question": 'Where is the final resting place of Napoleon Bonaparte?',
+        "option1": 'Island of Saint Helena',
+        "option2": 'Paris, France',
+        "option3": 'Moscow, Russia',
+        "option4": 'Dungeons on the island of Elba',
+        "answer": "2"
+    }
+]
 
 
 /*------------------------------------------------------- CSS RELATED GAME VARIABLES -------------------------------------------------------------*/
+const startButton = document.getElementById('start');
+startButton.addEventListener('click', startGame)
 const nextButton = document.getElementById('next');
 let submitFlex = document.querySelector('.submit');
 const questionContainer = document.querySelector('.question-container');
 const container = document.querySelector('.container');
+const displayScore = document.getElementById('score-result');
+const napoleonQuiz = document.querySelector('.napoleon-h1')
 
 
 /* -------------------------------------------------------Question Related ------------------------------------------------------------------*/
+var currentQuestion = 0;
+var score = 0;
+var totQuestions = questions.length;
 
-let shuffledQuestions, currentQuestionIndex
-const questionElement = document.getElementById('question')
-const choice = document.querySelectorAll('value');
-
-
-
-
-
+var mainContainer = document.getElementById('main-flexbox');
+var questionEl = document.getElementById('question');
+var opt1 = document.getElementById('opt1');
+var opt2 = document.getElementById('opt2');
+var opt3 = document.getElementById('opt3');
+var opt4 = document.getElementById('opt4');
+var advanceButton = document.getElementById('next');
+var resultCont = document.getElementById('score');
 
 
 
@@ -31,82 +78,90 @@ function startGame() {
     questionContainer.style.display ='initial'; // Makes Question Container appear
     container.style.display ='grid'; // Makes GRID Of answers appear
     nextButton.style.display = 'initial'; // Makes next button visible
-    submitFlex.style.justifyContent = 'flex-end'; // Makes next button protrude towards the right side of the screen
-    shuffledQuestions = questions.sort(() => Math.random() - .5);
-    currentQuestionIndex = 0;
-    setNextQuestion();
-}
-
-function setNextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
-}
-
-
-
-
-
-
-function showQuestion(question) {
-    questionElement.innerText = question.question;
-    
-        
-
-    
-        
-       
-
-    
+    submitFlex.style.justifyContent = 'flex-end'; // Makes next button protrude towards the right side of the screen 
+    displayScore.style.visibility = 'hidden';
+    loadQuestion(currentQuestion);
     
 }
 
 
 
 
-function selectAnswer() {
+function loadQuestion (index) {
+    var q = questions[index];  // This means var q will select index from questions array ,Index 0 is first set of questions and answers. //
+    questionEl.textContent = (index + 1) + '. ' + q.question; // Tracks the index of the question , Example 1. Question 1 , 2. Question 2 , Etc.
+    opt1.textContent = q.option1;
+    opt2.textContent = q.option2;
+    opt3.textContent = q.option3;
+    opt4.textContent = q.option4;
+};
 
-
-}
-
-
-
-
-
-
-const questions = [
-    {
-        question: 'When did Napoleon Bonaparte become the Emperor of France.',
-        answers: [
-            {text: '1804', correct: true},
-            {text: '1815', correct: false},
-            {text: '1705', correct: false},
-            {text: '1920', correct: false}
-        ]
-    },
-    {
-        question: 'What year did Napoleon Bonaparte invade Russia?',
-        answers: [
-            {text: '1819', correct: false},
-            {text: '1812', correct: true},
-            {text: '1925', correct: false},
-            {text: '1710', correct: false}
-        ]
-    },
-    {
-        question: 'To What island was Napoleon exiled, When he first abdicated the throne?',
-        answers: [
-            {text: 'Island of Elba', correct: true},
-            {text: 'Island Dutchy of Touissant', correct: false},
-            {text: 'Saint Pierre', correct: false},
-            {text: 'Island of Rivia', correct: false}
-        ]
-    },
-    {
-        question: 'Who led the army that defeated Napoleon Bonaparte , At the battle of Waterloo?',
-        answers: [
-            {text: 'Geralt of Rivia', correct: false},
-            {text: 'Jean Pierre De La Rouge', correct: false},
-            {text: 'Kaiser Henry the 3rd', correct: false},
-            {text: 'Arthur Wellesley , The duke of Wellington', correct: true}
-        ]
+function loadNextQuestion() {
+    var selectedOption = document.querySelector('input[type=radio]:checked');
+    if(!selectedOption){
+        alert('Please select your answer!');
+        return;
     }
-]
+
+
+    var answer = selectedOption.value;
+    if(questions[currentQuestion].answer == answer){
+        score += 10;
+    }
+    
+
+    selectedOption.checked = false;
+    currentQuestion++;
+
+    if(currentQuestion == totQuestions - 1){
+        advanceButton.textContent = 'Finish'; 
+    }
+
+
+    if(currentQuestion == totQuestions){
+        container.style.display = 'none';
+        displayScore.style.visibility = 'initial';
+        questionContainer.style.display = 'none';
+        resultCont.style.display = '';
+        resultCont.textContent = score + ' /50';
+        advanceButton.textContent = 'Game Completed!'
+        
+        if(score == 50) {
+            napoleonQuiz.innerHTML = 'You win!';
+            advanceButton.style.pointerEvents = 'none'
+            document.body.style.backgroundImage = "url('../images/napo.png')";
+            document.body.style.backgroundSize = "cover";
+            document.body.style.backgroundPosition = "center top";
+        } else {
+            napoleonQuiz.innerHTML = ': ( Sorry better luck next time!'
+            advanceButton.style.pointerEvents = 'none'
+            document.body.style.backgroundImage = "url('../images/batttle.jpeg')";
+            document.body.style.backgroundSize = "contain";
+            document.body.style.backgroundPosition = "center center";
+
+        }
+        
+        return;
+    }
+    loadQuestion(currentQuestion);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
